00accbc1d3da47cd28f339333f75dcdf8a31f5ef
Svyatoslav Bezgin <kirdyk@rcbd.org>
2011-06-14 02:51:05 +0400

    Changed X function to be matrix.

--- pendulum.ml
+++ pendulum.ml
@@ -30,62 +30,37 @@ open Print;;
 open Bigarray;;
 open Matrix;;
 
-let array2_display print newline b = 
-    for i = 0 to Array2.dim1 b - 1 do
-        for j = 0 to Array2.dim2 b - 1 do
-            print b.{i,j}
-        done;
-        newline();
-    done;
-;;
-
-let some_print b = 
-    match b with
-    (x , y, z) ->
-        Printf.printf "%f\t" x;
-        for i = 0 to Array2.dim1 y - 1 do
-            for j = 0 to Array2.dim2 y - 1 do
-                Printf.printf " %f\t" y.{i,j}
-           done;
-        done;
-        for i = 0 to Array2.dim1 z - 1 do
-            for j = 0 to Array2.dim2 z - 1 do
-                Printf.printf " %f\t" z.{i,j}
-           done;
-        done;
-        print_newline ();
-;;
-
-let k_diff a b q r t k_now = 
-    let at = a t in
-    let at' = transpose at
+let k_magnit_diff a b q r t k_now = 
+    let a' = transpose a
     and b' = transpose b in
-    let k_diff1 = -1. &*. (k_now @*. at) 
-    and k_diff2 = at' @*. k_now 
+    let k_diff1 = -1. &*. (k_now @*. a) 
+    and k_diff2 = a' @*. k_now 
     and k_diff3 = (1./.r) &*. (((k_now @*. b) @*. b') @*. k_now) in
     ((k_diff1 @-. k_diff2) @+. k_diff3) @-. q
 ;;
 
-let g_diff a phi h b q r k t g_now = 
-    let at = a t 
-    and b' = transpose b 
-    and phit = phi t
-    and ht = h t in
-    let g_diff_part = (b @*. b') @*. k in
-    let g_diff1 = (transpose (at @-. g_diff_part)) @*. g_now
-    and g_diff2 = q @*. phit
-    and g_diff3 = k @*. ht in
-    ((-.1./.r) &*. g_diff1) @-. g_diff2 @+. g_diff3
+
+let a_magnit a d = Array2.of_array float64 c_layout [|
+    [| 0. ; 0. ; 0. |];
+    [| 1. ; 0. ; 0. |];
+    [| -.a*.d; a; 0. |] |]
 ;;
 
-   
-let x_diff a b d sigma delta t x_now = Array2.of_array float64 c_layout [|
-    [| b *. x_now.{0,0} *. ((1. -. sigma) *. x_now.{2,0} -. delta *. x_now.{1,0})|];
-    [| x_now.{0,0} *. (1. -.
-                       (1. -. delta) *. x_now.{1,0} +.
-                       sigma *. x_now.{2,0}) |];
-    [| a *. (x_now.{1,0} -. d *. x_now.{0,0})|]
-    |]
+let b_magnit = Array2.of_array float64 c_layout [|
+    [| 1. |];
+    [| 0. |];
+    [| 0. |]|]
+;;
+
+let f_magnit b sigma delta x u = Array2.of_array float64 c_layout [|
+    [| b *. x.{0,0} *. ((1. -. sigma) *. x.{2,0} -. delta *. x.{1,0})|];
+    [| x.{0,0} *. ((delta -. 1.) *. x.{1,0} +. sigma *. x.{2,0}) |];
+    [| 0. |]|]
+;;
+
+
+let x_magnit_diff a b f u t x = 
+    ((a @*. x) @+. (u &*. b)) @+. (f x u)
 ;;
 
 let x_0 = Array2.of_array float64 c_layout [|
@@ -118,6 +93,13 @@ let solve' k g k0 g0 t0 h t_end =
     rk4_rec [] k g k0 g0 t0 h t_end
 ;;
 
+let q_r = Array2.of_array float64 c_layout [|
+    [| 1. ; 0. ; 0. |];
+    [| 0. ; 0. ; 0. |];
+    [| 0. ; 0. ; 0. |] |]
+;;
+
+
 let print_result b = 
     match b with
     (x , y) ->
@@ -136,11 +118,20 @@ let _ =
     let a = 7.
     and b = 0.4
     and d = 1.17 
+    and r_r = 0.5
+    and k_0 = q_r
     and sigma = 0.284
     and delta = 0.681 in
-    let f_x' = x_diff a b d sigma delta in
+    let a_r = a_magnit a d
+    and b_r = b_magnit
+    and f_r = f_magnit b sigma delta in
+    let f_x' = x_magnit_diff a_r b_r f_r 0. in
+    let result = mx_rk4_down (k_magnit_diff a_r b_r q_r r_r) k_0 1. 0.001 0. in
+    List.map (print_result) result
+    (*
     let result = mx_rk4_up f_x' x_0 0. 0.001 30. in
     List.map (print_result) result
+    *)
     
 (*    List.map (some_print) result *)
 (*    let k_diff = mx_rk4_step k f_r 1.0 (-0.0005) in
